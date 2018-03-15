8b52e10306ab0b9f90e6e689c2e5eee455e99976
YiotaEB <35492416+YiotaEB@users.noreply.github.com>
2018-03-15 19:01:23 +0200

    Implementing the Suppliers  and Customers Fragments

---
+++ EB_AndroidApp/eb_androidapp/app/src/main/java/com/easybusiness/eb_androidapp/AsyncTask/ViewCustomers_SelectProductsAsyncTask.java
@@ -0,0 +1,132 @@
+package com.easybusiness.eb_androidapp.AsyncTask;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ListView;
+
+import com.easybusiness.eb_androidapp.Entities.Products;
+import com.easybusiness.eb_androidapp.R;
+import com.easybusiness.eb_androidapp.UI.Adapters.ProductAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/*
+
+Sample query builder:
+
+Uri.Builder builder = new Uri.Builder()
+                .appendQueryParameter("username", edit_username.getText().toString())
+                .appendQueryParameter("password", edit_password.getText().toString());
+
+            String query = builder.build().getEncodedQuery();
+ */
+
+public class ViewCustomers_SelectProductsAsyncTask extends AsyncTask<Void,Void,Void> {
+
+    private String query;
+    private String responseData;
+    private Activity activity;
+    private View view;
+    ArrayList<Products> products = null;
+
+    public ViewCustomers_SelectProductsAsyncTask(String query, Activity activity, View view) {
+        this.query = query;
+        this.activity = activity;
+        this.view = view;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        if (query == null) query = "";
+
+        try {
+            URL url = new URL(AsyncTasks.encodeForAPI(activity.getString(R.string.baseURL), "Products", "GetMultiple"));
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            byte[] outputBytes = query.getBytes("UTF-8");
+            urlConnection.setRequestMethod("POST");
+            urlConnection.connect();
+            OutputStream os = urlConnection.getOutputStream();
+            os.write(outputBytes);
+            os.close();
+            int statusCode = urlConnection.getResponseCode();
+            urlConnection.disconnect();
+
+            //OK
+            if (statusCode == HttpURLConnection.HTTP_OK) {
+                InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                responseData = AsyncTasks.convertStreamToString(inputStream);
+
+                JSONObject outterObject = new JSONObject(responseData);
+                final String status = outterObject.getString("Status");
+                final String title = outterObject.getString("Title");
+                final String message = outterObject.getString("Message");
+
+                if (status.equals(AsyncTasks.RESPONSE_OK)) {
+                    products = new ArrayList<>();
+                    JSONArray dataArray = outterObject.getJSONArray("Data");
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        JSONObject jsonObject = dataArray.getJSONObject(i);
+                        String name = jsonObject.getString("Name");
+                        int quantityInStock = jsonObject.getInt("QuantityInStock");
+                        Products p = new Products(0, name, 0, quantityInStock, 0, 0, 0);
+                        products.add(p);
+                    }
+
+                    final ListView productsListview = activity.findViewById(R.id.product_list);
+                    String [] items = new String[products.size()];
+                    for (int i = 0; i < items.length; i++)
+                        items[i] = products.get(i).getName();
+                    final ProductAdapter productAdapter = new ProductAdapter(activity, products);
+
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            productsListview.setAdapter(productAdapter);
+                        }
+                    });
+
+
+                }
+                else if (status.equals(AsyncTasks.RESPONSE_ERROR)) {
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            final AlertDialog alertDialog = AsyncTasks.createUnknownErrorDialog(activity, title, message);
+                            alertDialog.show();
+                        }
+                    });
+                }
+
+
+            }
+            //CONNECTION ERROR
+            else {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final AlertDialog alertDialog = AsyncTasks.createConnectionErrorDialog(activity);
+                        alertDialog.show();
+                    }
+                });
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
---
+++ EB_AndroidApp/eb_androidapp/app/src/main/java/com/easybusiness/eb_androidapp/UI/Fragments/SelectProductsFragment.java
@@ -0,0 +1,58 @@
+package com.easybusiness.eb_androidapp.UI.Fragments;
+
+
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.easybusiness.eb_androidapp.AsyncTask.GetProductsAsyncTask;
+import com.easybusiness.eb_androidapp.AsyncTask.ViewCustomers_SelectProductsAsyncTask;
+import com.easybusiness.eb_androidapp.R;
+import com.easybusiness.eb_androidapp.UI.MainActivity;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SelectProductsFragment extends Fragment {
+
+    public static final String TAG = "SelectProductsFragment";
+    public static final String TITLE = "Select Products";
+
+    View view;
+
+    public SelectProductsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_select_products, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sessionID = sharedPreferences.getString(MainActivity.PREFERENCE_SESSIONID, "None");
+
+        Uri.Builder builder = new Uri.Builder().appendQueryParameter("SessionID", sessionID);
+        String query = builder.build().getEncodedQuery();
+
+        new ViewCustomers_SelectProductsAsyncTask(query, getActivity(), view).execute();
+
+    }
+
+}
