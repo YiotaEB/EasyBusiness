44b188716d990d5b82be09601a916247b1f380a1
Panayiota <panayiota95b@hotmail.co.uk>
2018-05-14 22:23:53 +0300

    Final

--- EB_AndroidApp/eb_androidapp/app/src/main/java/com/easybusiness/eb_androidapp/UI/Fragments/UserHomeFragment.java
+++ EB_AndroidApp/eb_androidapp/app/src/main/java/com/easybusiness/eb_androidapp/UI/Fragments/UserHomeFragment.java
@@ -9,15 +9,20 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.easybusiness.eb_androidapp.AsyncTask.AsyncTasks;
 import com.easybusiness.eb_androidapp.Entities.Countries;
+import com.easybusiness.eb_androidapp.Entities.Customers;
+import com.easybusiness.eb_androidapp.Entities.Days;
 import com.easybusiness.eb_androidapp.Entities.Products;
 import com.easybusiness.eb_androidapp.Entities.SaleProducts;
 import com.easybusiness.eb_androidapp.Entities.Sales;
@@ -35,6 +40,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -54,18 +60,17 @@ public class UserHomeFragment extends Fragment {
 
 
     private ArrayList<Sales> salesList;
-    private ArrayList<Countries> countriesList;
-
-
-
+    private ArrayList<Customers> customersList;
 
     private ProgressBar progressBar;
     private CardView cardView;
+    private TextView companyNameTextView;
+    private TextView lastSaleTextView;
+    private Button addSaleButton;
+    private Button addCustomerButton;
+    private View layout;
+
     private String companyName;
-    private String companyAddress;
-    private String companyCity;
-    private String companyTelephone;
-    private String companyCountry;
 
     public UserHomeFragment() {
         // Required empty public constructor
@@ -80,9 +85,37 @@ public class UserHomeFragment extends Fragment {
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         sessionID = sharedPreferences.getString(MainActivity.PREFERENCE_SESSIONID, "");
 
+        layout = v.findViewById(R.id.company_info_cardview);
+        companyNameTextView = v.findViewById(R.id.companyName);
         progressBar = v.findViewById(R.id.user_home_progress);
         cardView = v.findViewById(R.id.company_info_cardview);
+        lastSaleTextView = v.findViewById(R.id.latest_sale);
+        addSaleButton = v.findViewById(R.id.addSale);
+        addCustomerButton = v.findViewById(R.id.addCustomer);
 
+        addSaleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment newFragment = new AddSaleFragment();
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(R.anim.slide_left_to_right, R.anim.slide_right_to_left, R.anim.slide_left_to_right, R.anim.slide_right_to_left);
+                fragmentTransaction.replace(R.id.frame, newFragment, AddSaleFragment.TAG);
+                fragmentTransaction.addToBackStack(newFragment.getTag());
+                fragmentTransaction.commit();
+            }
+        });
+
+        addCustomerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment newFragment = new AddCustomersFragment();
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(R.anim.slide_left_to_right, R.anim.slide_right_to_left, R.anim.slide_left_to_right, R.anim.slide_right_to_left);
+                fragmentTransaction.replace(R.id.frame, newFragment, AddCustomersFragment.TAG);
+                fragmentTransaction.addToBackStack(newFragment.getTag());
+                fragmentTransaction.commit();
+            }
+        });
 
         return v;
     }
@@ -92,7 +125,6 @@ public class UserHomeFragment extends Fragment {
         super.onResume();
         getActivity().setTitle(TITLE);
 
-        new GetCountriesAsyncTask().execute();
         new GetSalesAsyncTask().execute();
 
     }
@@ -141,24 +173,11 @@ public class UserHomeFragment extends Fragment {
                             JSONObject jsonObject = dataArray.getJSONObject(i);
 
                             companyName = jsonObject.getString("CompanyName");
-                            companyAddress = jsonObject.getString("Address");
-                            companyCity = jsonObject.getString("City");
-                            companyTelephone = jsonObject.getString("Telephone");
-
-                            int countryID = jsonObject.getInt("CountryID");
-
-                            for (int j = 0; j < countriesList.size(); j++) {
-                                if (countryID == countriesList.get(j).getID()) {
-                                    companyCountry = countriesList.get(j).getName();
-                                    break;
-                                }
-                            }
 
                             getActivity().runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    cardView.setVisibility(View.VISIBLE);
-                                    progressBar.setVisibility(View.GONE);
+                                    companyNameTextView.setText(companyName);
                                 }
                             });
 
@@ -194,102 +213,9 @@ public class UserHomeFragment extends Fragment {
             return null;
         }
 
-    }
-
-    private class GetCountriesAsyncTask extends AsyncTask<Void, Void, Void> {
-
-        private String responseData;
-        private String query;
-
-        public GetCountriesAsyncTask() {
-            Uri.Builder builder = new Uri.Builder()
-                    .appendQueryParameter("Limit", String.valueOf("0"))
-                    .appendQueryParameter("SessionID", PreferenceManager.getDefaultSharedPreferences(getActivity()).getString(MainActivity.PREFERENCE_SESSIONID, ""));
-            query = builder.build().getEncodedQuery();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    cardView.setVisibility(View.GONE);
-                    progressBar.setVisibility(View.VISIBLE);
-                }
-            });
-
-            countriesList = new ArrayList<>();
-
-
-            try {
-                URL url = new URL(AsyncTasks.encodeForAPI(getActivity().getString(R.string.baseURL), "Countries", "GetMultiple"));
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setDoOutput(true);
-                byte[] outputBytes = query.getBytes("UTF-8");
-                urlConnection.setRequestMethod("POST");
-                urlConnection.connect();
-                OutputStream os = urlConnection.getOutputStream();
-                os.write(outputBytes);
-                os.close();
-                int statusCode = urlConnection.getResponseCode();
-                urlConnection.disconnect();
-
-                //OK
-                if (statusCode == HttpURLConnection.HTTP_OK) {
-                    InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
-                    responseData = AsyncTasks.convertStreamToString(inputStream);
-
-                    JSONObject outterObject = new JSONObject(responseData);
-
-                    final String status = outterObject.getString("Status");
-                    final String title = outterObject.getString("Title");
-                    final String message = outterObject.getString("Message");
-
-                    if (status.equals(AsyncTasks.RESPONSE_OK)) {
-                        final JSONArray dataArray = outterObject.getJSONArray("Data");
-                        for (int i = 0; i < dataArray.length(); i++) {
-                            JSONObject jsonObject = dataArray.getJSONObject(i);
-                            int id = jsonObject.getInt("ID");
-                            final String name = jsonObject.getString("Name");
-                            Countries c = new Countries(id, name);
-                            countriesList.add(c);
-                        }
-
-                    } else if (status.equals(AsyncTasks.RESPONSE_ERROR)) {
-                        getActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                final AlertDialog alertDialog = AsyncTasks.createGeneralErrorDialog(getActivity(), title, message);
-                                alertDialog.show();
-                            }
-                        });
-                    }
-
-                    //CONNECTION ERROR
-                    else {
-                        getActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                final AlertDialog alertDialog = AsyncTasks.createConnectionErrorDialog(getActivity());
-                                alertDialog.show();
-                            }
-                        });
-                    }
-
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return null;
-
-        }
-
         @Override
         protected void onPostExecute(Void aVoid) {
-            new GetCompanyInformationAsyncTask().execute();
+            new GetCustomersAsyncTask().execute();
         }
     }
 
@@ -310,6 +236,14 @@ public class UserHomeFragment extends Fragment {
             query = builder.build().getEncodedQuery();
 
             if (query == null) query = "";
+
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    progressBar.setVisibility(View.VISIBLE);
+                    layout.setVisibility(View.GONE);
+                }
+            });
 
             try {
                 URL url = new URL(AsyncTasks.encodeForAPI(getActivity().getString(R.string.baseURL), "Sales", "GetMultiple"));
@@ -378,7 +312,136 @@ public class UserHomeFragment extends Fragment {
             }
             return null;
         }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            new GetCompanyInformationAsyncTask().execute();
+        }
     }
+
+    public class GetCustomersAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private String query;
+        private String responseData;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            customersList = new ArrayList<>();
+
+            Uri.Builder builder = new Uri.Builder()
+                    .appendQueryParameter("Limit", "0")
+                    .appendQueryParameter("SessionID", sessionID);
+
+            query = builder.build().getEncodedQuery();
+
+            if (query == null) query = "";
+
+            try {
+                URL url = new URL(AsyncTasks.encodeForAPI(getActivity().getString(R.string.baseURL), "Customers", "GetMultiple"));
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                byte[] outputBytes = query.getBytes("UTF-8");
+                urlConnection.setRequestMethod("POST");
+                urlConnection.connect();
+                OutputStream os = urlConnection.getOutputStream();
+                os.write(outputBytes);
+                os.close();
+                int statusCode = urlConnection.getResponseCode();
+                urlConnection.disconnect();
+
+                //OK
+                if (statusCode == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                    responseData = AsyncTasks.convertStreamToString(inputStream);
+
+                    JSONObject outterObject = new JSONObject(responseData);
+                    final String status = outterObject.getString("Status");
+                    final String title = outterObject.getString("Title");
+                    final String message = outterObject.getString("Message");
+
+                    if (status.equals(AsyncTasks.RESPONSE_OK)) {
+                        JSONArray dataArray = outterObject.getJSONArray("Data");
+                        for (int i = 0; i < dataArray.length(); i++) {
+                            JSONObject jsonObject = dataArray.getJSONObject(i);
+
+                            final int id = jsonObject.getInt("ID");
+                            final String name = jsonObject.getString("Name");
+                            int countryID = jsonObject.getInt("CountryID");
+                            String city = jsonObject.getString("City");
+                            String address = jsonObject.getString("Address");
+                            String telephone = jsonObject.getString("Telephone");
+                            int customerProductsID = jsonObject.getInt("CustomerProductsID");
+
+                            Customers p = new Customers(id, name, countryID, city, address, telephone, customerProductsID);
+                            customersList.add(p);
+                        }
+
+                        String customerName = "";
+                        long time = 0;
+                        for (int i = 0; i < customersList.size(); i++) {
+                            if (salesList.get(salesList.size() - 1).getCustomerID() == customersList.get(i).getID()) {
+                                customerName = customersList.get(i).getName();
+                                System.out.println(customerName);
+                                time = salesList.get(salesList.size() - 1).getSaleTimeDate();
+                                break;
+                            }
+                        }
+                        Date date = new Date(time);
+                        String formattedTime = Days.DATE_FORMAT.format(date) + " at " + Days.TIME_FORMAT.format(date);
+                        final String lastSale = "To " + customerName + " on: " + formattedTime;
+
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                lastSaleTextView.setText(lastSale);
+
+                            }
+                        });
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressBar.setVisibility(View.GONE);
+                                layout.setVisibility(View.VISIBLE);
+                            }
+                        });
+
+
+
+                    } else if (status.equals(AsyncTasks.RESPONSE_ERROR)) {
+                        final AlertDialog alertDialog = AsyncTasks.createGeneralErrorDialog(getActivity(), title, message);
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                alertDialog.show();
+                            }
+                        });
+                    }
+
+
+                }
+                //CONNECTION ERROR
+                else {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            final AlertDialog alertDialog = AsyncTasks.createConnectionErrorDialog(getActivity());
+                            alertDialog.show();
+                        }
+                    });
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+    }
+
 
 
 }
